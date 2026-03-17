246d611a8f4b596f48e4bbc6c399e53b9dee4c3f
Splendid Innate <spleinnate@gmail.com>
2026-03-17 14:18:40 +0200

    Add files via upload

---
+++ Level 1 – Python Basics for Cyber Security/L1T11 – Control Structures – For Loop/pattern.py
@@ -0,0 +1,10 @@
+#L1T11 Practical Task:
+
+#For Loop
+for i in range(1, 10):
+    
+    # if-else statement to control the increasing and decreasing of pattern
+    if i <= 5:
+        print("*" * i)      #Increasing part
+    else:
+        print("*" * (10 - i))  #Decreasing part
