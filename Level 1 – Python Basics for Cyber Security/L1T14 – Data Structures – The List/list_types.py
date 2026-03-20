2d86f8b288de10a0a6f28619e7637d7100aed7f4
Splendid Innate <spleinnate@gmail.com>
2026-03-20 16:49:52 +0200

    Add files via upload

---
+++ Level 1 – Python Basics for Cyber Security/L1T14 – Data Structures – The List/list_types.py
@@ -0,0 +1,10 @@
+#Auto-Grade Task:
+
+friends_names = ["Tumelo", "Morobela", "Naledi"]
+print(f"The first word in the list is: {friends_names[0]}, and the last word is: {friends_names[-1]}, and the length of the list is: {len(friends_names)}")
+print(" ")
+
+friends_ages = [22, 25, 23]
+print(f"{friends_names[0]} is {friends_ages[0]} years old.")
+print(f"{friends_names[1]} is {friends_ages[1]} years old.")
+print(f"{friends_names[2]} is {friends_ages[2]} years old.")
